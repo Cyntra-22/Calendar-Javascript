a4c31cae77093aa248063d6047e8b1301696047a
Cyntra-22 <140875968+Cyntra-22@users.noreply.github.com>
2023-09-06 11:04:08 +0700

    Updated the js file and html with functions

--- src/function.js
+++ src/function.js
@@ -1,10 +1,83 @@
-document.writeln("<table>");
-document.writeln("<caption> August 2023 Calendar</caption>");
-document.writeln("<thead><tr><th>Sun</th>");
-document.writeln("<th>Mon</th>");
-document.writeln("<th>Tue</th>");
-document.writeln("<th>Wed</th>");
-document.writeln("<th>Thu</th>");
-document.writeln("<th>Fri</th>");
-document.writeln("<th>Sat</th>");
-document.writeln("</tr></thead><tbody>");
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const calendarContainer = document.getElementById("calendar-container");
+const prevMonthButton = document.getElementById("prev-month-btn");
+const nextMonthButton = document.getElementById("next-month-btn");
+
+const daysOfMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+let currentMonthIndex = 0;
+
+function showMonthOf2023(monthIndex) {
+  const currentYear = 2023;
+  const daysInMonth = daysOfMonth[monthIndex];
+  const monthName = monthNames[monthIndex];
+
+  calendarContainer.innerHTML = "";
+
+  const header = document.createElement("h2");
+  header.textContent = `${monthName} ${currentYear}`;
+  calendarContainer.appendChild(header);
+
+  const table = document.createElement("table");
+  table.classList.add("calendar-table");
+
+  const headerRow = document.createElement("tr");
+  for (let dayOfWeek = 0; dayOfWeek < 7; dayOfWeek++) {
+    const th = document.createElement("th");
+    th.textContent = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"][
+      dayOfWeek
+    ];
+    headerRow.appendChild(th);
+  }
+  table.appendChild(headerRow);
+
+  const firstDay = new Date(currentYear, monthIndex, 1).getDay();
+
+  let currentDate = 1;
+  for (let i = 0; i < 6; i++) {
+    const row = document.createElement("tr");
+    for (let j = 0; j < 7; j++) {
+      const cell = document.createElement("td");
+      if (i === 0 && j < firstDay) {
+        cell.textContent = "";
+      } else if (currentDate <= daysInMonth) {
+        cell.textContent = currentDate;
+        currentDate++;
+      }
+      row.appendChild(cell);
+    }
+    table.appendChild(row);
+  }
+
+  calendarContainer.appendChild(table);
+}
+
+showMonthOf2023(currentMonthIndex);
+
+prevMonthButton.addEventListener("click", () => {
+  if (currentMonthIndex > 0) {
+    currentMonthIndex--;
+    showMonthOf2023(currentMonthIndex);
+  }
+});
+
+nextMonthButton.addEventListener("click", () => {
+  if (currentMonthIndex < 11) {
+    currentMonthIndex++;
+    showMonthOf2023(currentMonthIndex);
+  }
+});
